a6cedba14d3a7514d9ed1c5d5cb84700a3b4caf7
정연수 <jys1015@hanyang.co.kr>
2020-02-13 14:17:39 +0900

    class in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/*
+//interface는 js로 컴파일되지않는다 js에서 확인하려면 class를 이용한다.
 interface Human {
     name:string;
     age:number;
@@ -10,11 +12,25 @@ const person = {
     gender: "male"
 }
 
+*/
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name:string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
+const ys = new Human("Yeonsu",36,"male");
+
 // return 은 :를이요하여 정한다.
 const sayHi = (person:Human):string => {
     return `Hello ${person.name}, you are ${person.age}, gender is ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(ys));
 
 export {};
