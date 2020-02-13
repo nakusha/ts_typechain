6f5d68856197e0e7528fcfda12a8570b1328604c
정연수 <jys1015@hanyang.co.kr>
2020-02-13 16:22:11 +0900

    prepare block chain

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import * as CryptoJS from "crypto-js";
+
 class Block { 
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string):string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
 
     constructor(index: number,
         hash: string,
@@ -19,15 +23,19 @@ class Block {
         }
 }
 
+
 const genesisBlock:Block = new Block(0, "123gvi2b1bd", "", "hello", 123456);
 
-let blockchain:[Block] = [genesisBlock];
+let blockchain:Block[] = [genesisBlock];
 
 // Block 배열에 string가 들어가려해서 ts에서 자동으로 애러처리함
 //blockchain.push("stuff");
 
-console.log(blockchain);
+const getBlockchain = () : Block[] => blockchain;
 
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 
 export {};
